d75fedd021e48edabd0b927aa1ef7e88a5e3c4e3
matthew <matthewevans300@gmail.com>
2018-09-06 16:43:07 -0500

    9/6 updates -- chrome storage.set not working

--- content.js
+++ content.js
@@ -2,18 +2,36 @@ console.log("content.js is running");
 
 var value = "some data";
 
-//storage stuff
-chrome.storage.sync.set({key: value}, function() {
-    console.log('Value is set to ' + value);
-});
-chrome.storage.sync.get(['key'], function(result) {
-    console.log('Value currently is ' + result.key);
-});
+var lastHostName;
+var timeSpent = 0;
+ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+       var url = new URL(tabs[0].url);
+       lastHostName = url.hostname;
+   });
+
+var d = new Date();
+var startTime = d.getTime();
 
 chrome.tabs.onUpdated.addListener(function() {
   console.log("Tab updated.");
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
       var url = new URL(tabs[0].url);
+
+      // console.log("Last host name" + lastHostName);
+      // console.log("Current URL name" + url.hostname);
+
+
+      if(lastHostName != url.hostname) {
+        lastHostName = url.hostname;
+        d = new Date();
+        timeSpent = d.getTime() - startTime;
+        startTime = d.getTime();
+
+        logTime(url.hostname, timeSpent);
+
+
+      }
+
       console.log(url);
       console.log("url hostname is "+ url.hostname);
   });
@@ -23,10 +41,28 @@ chrome.tabs.onActivated.addListener(function() {
   console.log("Tab activated.");
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
       var url = new URL(tabs[0].url);
-      console.log(url);
-      console.log("url hostname is "+ url.hostname);
+      //console.log(url);
+      //console.log("url hostname is "+ url.hostname);
   });
 });
+
+function logTime(hostname, time) {
+    //check if hostname key exists
+
+   chrome.storage.local.get([hostname], function(result) {
+      console.log("Value is "+ result.value);
+      console.log("Key is "+ result.key);
+      if(result.key == undefined) {
+        chrome.storage.local.set({hostname: time}, function() {
+            console.log("first time you left "+hostname+" created new key with a new time of "+ time);
+        });
+      } else {
+        chrome.storage.local.set({hostname: (time + result.key)}, function() {
+            console.log("Time is updated to "+ (time + result.key) +" for "+ hostname);
+        });
+      }
+   });
+}
 
 
 
